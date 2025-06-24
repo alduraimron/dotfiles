e35b0bf75785f06ae095d7aa9b53d089c6216d00
alduraimron <alduraarmanushaufaimron2@gmail.com>
2025-06-24 19:18:19 +0700

    add initial neovim config

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    --config = function()
+    --  local lspconfig = require("lspconfig")
+    --  lspconfig.lua_ls.setup({})
+    --  lspconfig.intelephense.setup({})
+    --end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+        },
+      })
+    end,
+  },
+}
+
---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme tokyonight")
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("neo-tree").setup()
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup {
+        options = {
+          theme = "auto",
+          section_separators = { left = "", right = "" },
+          component_separators = { left = "", right = "" },
+        },
+      }
+    end,
+  },
+}
+
